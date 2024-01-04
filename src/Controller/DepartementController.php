bd3d3649479f9f4eefb22ca2275656462421373c
=Fugushiva <2111jedelodder@student.epfc.eu>
2024-01-04 14:39:52 +0100

    ajout du formulaire pour ajouter un departement

--- src/Controller/DepartementController.php
+++ src/Controller/DepartementController.php
@@ -3,41 +3,57 @@
 namespace App\Controller;
 
 use App\Entity\Departement;
+use App\Form\DepartementType;
 use App\Repository\DepartementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
+
+
 
 //jerome
 class DepartementController extends AbstractController
 {
-    #[Route('/departement', name: 'app_departement', methods:['GET'])]
+    #[Route('/departement', name: 'app_departement_index', methods: ['GET'])]
     public function index(DepartementRepository $dr): Response
     {
         $departementsData = $dr->findAll();
-        
+
         return $this->render('departement/index.html.twig', [
             'controller_name' => 'DepartementController',
             "departements" => $departementsData,
-           
+
 
         ]);
     }
 
     #[Route('/new', name: 'app_departement_new')]
-    public function new(DepartementRepository $dr): Response
+    public function new(Request $request, EntityManagerInterface $em): Response
     {
 
+        $departement = new Departement();
+
+        $departementForm = $this->createForm(DepartementType::class, $departement);
+
+        $departementForm->handleRequest($request);
+
+        if ($departementForm->isSubmitted() && $departementForm->isValid()) {
+            $em->persist($departement);
+            $em->flush();
+
+            return $this->redirectToRoute('app_department_index', [], Response::HTTP_SEE_OTHER);
+        }
 
 
 
         return $this->render('departement/new.html.twig', [
-            'controller_name' => 'DepartementController',
-            
-           
+            'form' => $departementForm,
+
+
 
         ]);
     }
-
-  
 }
--- src/Form/DepartementType.php
+++ src/Form/DepartementType.php
@@ -8,22 +8,32 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
 
 class DepartementType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('deptNo')
+            ->add('deptNo', TextType::class, [
+                'constraints' => [
+                    new Regex([
+                        'pattern' => '/^d\d{3}$/',
+                        'message' => 'Le numéro de département doit être la lettre "d" suivie de 3 chiffres.',
+                    ]),
+                ],
+            ])
             ->add('deptName')
             ->add('description')
             ->add('adresse')
             ->add('roi')
-            ->add('manager', EntityType::class, [
-                'class' => DeptManager::class,
-'choice_label' => 'id',
-            ])
-        ;
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
