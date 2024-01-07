3c5c2f0fa2e2349b4a659e3229b978bb4d6463aa
Bybax07 <bilel.1998@hotmail.com>
2024-01-07 16:58:49 +0100

    Formulaire pour postuler pour chaque poste

--- src/Controller/DeptTitleController.php
+++ src/Controller/DeptTitleController.php
@@ -3,8 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\DeptTitle;
-use App\Entity\Departement;
-use App\Repository\DepartementRepository;
 use App\Repository\DeptTitleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -13,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DeptTitleController extends AbstractController
 {
+    //Affichage de tout les postes qui sont disponible 
     #[Route('/dept_title', name: 'app_dept_title')]
     public function index(DeptTitleRepository $dt): Response
     {
-       
+       //Le tableau qui va reunir toutes les datas
         $deptTitleData = $dt->findAll();
         
         return $this->render('dept_title/index.html.twig', [
@@ -25,14 +24,18 @@ class DeptTitleController extends AbstractController
         ]);
     }
 
-    //Afficher les postes vacants
-    #[Route('/dept_title', name: 'app_dept_title_show')]
-    public function show(EntityManagerInterface $dt): Response
+    //Affichage d'un seul poste en particulier
+    #[Route('/dept_title/{id}', 
+        name: 'app_dept_title_show')]
+    public function show(EntityManagerInterface $dt, int $id): Response
     {
+
+        $repository = $dt ->getRepository(DeptTitle::class);
+        $poste = $repository->find($id);
+
         return $this->render('dept_title/show.html.twig', [
-            'controller_name' => 'DeptTitleController',
-            
+            'title' => 'Le poste',  
+            "poste" => $poste
         ]);
     }
-
 }
